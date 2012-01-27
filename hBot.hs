e9de062d4dfde69e0c233f79b8168eb5712a4b47
Simon Gomizelj <simongmzlj@gmail.com>
2012-01-27 08:24:07 -0500

    Code cleanups and style changes.

--- hBot.hs
+++ hBot.hs
@@ -4,7 +4,8 @@ import System.IO
 import System.Time
 import System.Exit
 import Control.Monad.Reader
-import Control.OldException
+import Control.Exception
+import Control.Monad
 import Text.Printf
 import Prelude hiding (catch)
 
@@ -18,20 +19,19 @@ data Bot = Bot { socket :: Handle, startTime :: ClockTime }
 
 main :: IO ()
 main = bracket connect disconnect loop
-    where
-        disconnect = hClose . socket
-        loop st    = catch (runReaderT run st) (const $ return ())
+  where
+    disconnect = hClose . socket
+    loop       = runReaderT run
 
 connect :: IO Bot
 connect = notify $ do
     t <- getClockTime
-    h <- connectTo server $ PortNumber $ fromIntegral port
+    h <- connectTo server . PortNumber $ fromIntegral port
     hSetBuffering h NoBuffering
     return $ Bot h t
-    where
-        notify a = bracket_ (printf "Connecting to %s ..." server >> hFlush stdout)
-                            (putStrLn "done.")
-                            a
+  where
+    notify = bracket_ (printf "Connecting to %s ..." server >> hFlush stdout)
+                      (putStrLn "done.")
 
 run :: Net ()
 run = do
@@ -47,17 +47,15 @@ listen = do
         s <- init `fmap` liftIO (hGetLine h)
         liftIO $ putStrLn s
         handled <- prot $ words s
-        if handled
-           then return ()
-           else eval (user s) (words $ clean s)
-    where
-        forever a = a >> forever a
-        clean     = drop 1 . dropWhile (/= ':') . drop 1
-        user      = drop 1 . takeWhile (/= '!')
+        unless handled $ eval (user s) (words $ clean s)
+  where
+    forever a = a >> forever a
+    clean     = drop 1 . dropWhile (/= ':') . drop 1
+    user      = drop 1 . takeWhile (/= '!')
 
 prot :: [String] -> Net Bool
-prot ("PING":xs)   = write "PONG" (unwords xs) >> return True
-prot _             = return False
+prot ("PING":xs) = write "PONG" (unwords xs) >> return True
+prot _           = return False
 
 eval :: String -> [String] -> Net ()
 eval _ ("PING":xs)   = write "PONG" (unwords xs)
@@ -87,12 +85,12 @@ pretty td = join . intersperse " " . filter (not . null) . map f $
     [ (years         , "y"), (months `mod` 12, "m")
     , (days  `mod` 28, "d"), (hours  `mod` 24, "h")
     , (mins  `mod` 60, "m"), (secs   `mod` 60, "s") ]
-    where
-        secs   = abs $ tdSec td
-        mins   = secs   `div` 60
-        hours  = mins   `div` 60
-        days   = hours  `div` 24
-        months = days   `div` 28
-        years  = months `div` 12
-        f (i,s) | i == 0    = []
-                | otherwise = show i ++ s
+  where
+    secs   = abs $ tdSec td
+    mins   = secs   `div` 60
+    hours  = mins   `div` 60
+    days   = hours  `div` 24
+    months = days   `div` 28
+    years  = months `div` 12
+    f (i,s) | i == 0    = []
+            | otherwise = show i ++ s
