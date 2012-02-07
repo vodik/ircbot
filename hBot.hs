63bf6fd106bf3e63292a9aad3d7f58fbdb944d37
Simon Gomizelj <simongmzlj@gmail.com>
2012-02-06 19:07:43 -0500

    Stats should be collected from everyone, not just authorized users.

--- hBot.hs
+++ hBot.hs
@@ -16,11 +16,11 @@ myNick   = "netopir"
 myChans  = [ "#bots", "#derpgmzlj" ]
 
 myProc stats msg@(Message p _ _) = case p of
-    Just (Nick u _ _) -> authorize u $ mconcat
+    Just (Nick u _ _) -> (authorize u $ mconcat
         [ runMacros msg $ mconcat [ eval, ids ]
-        , collectStats stats msg
-        ]
-    _                 -> return ()
+        ])
+        <+> collectStats stats msg
+    _ -> return ()
 
 changeNick n = send (nick n) >> modify (\s -> s { nick' = n })
 
